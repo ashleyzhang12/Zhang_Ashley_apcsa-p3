ad5b1e7d044850bed2d3501b3c164405e377f209
ashleyzhang12 <38962806+ashleyzhang12@users.noreply.github.com>
2019-02-01 12:50:55 -0800

    Added Unit02 files

---
+++ Unit02/src/Line.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( int x1, int y1, int x2, int y2 )
+	{
+		return ((float)y2 - y1) / (x2 - x1);
+	}
+
+}
