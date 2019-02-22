5069c94529c36202593ca18348cae55bb1e256ba
ashleyzhang12 <38962806+ashleyzhang12@users.noreply.github.com>
2019-02-22 12:11:54 -0800

    Added Unit08 files

--- Unit08/src/DownRunner.java
+++ Unit08/src/DownRunner.java
@@ -7,6 +7,8 @@ public class DownRunner
 {
 	public static void main( String args[] )
 	{
-		
+		System.out.println(RayDown.go(new int[] {-99,1,2,3,4,5,6,7,8,9,10,12345}));
+		System.out.println(RayDown.go(new int[] {255,255}));
+		System.out.println(RayDown.go(new int [] {10,9,8,7,6,5,4,3,2,1,-99}));
 	}
 }
--- Unit08/src/RayDown.java
+++ Unit08/src/RayDown.java
@@ -9,6 +9,18 @@ public class RayDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
-		return true;
+		boolean goingDown = false;
+		for (int i = 0; i < numArray.length - 1; i++)
+		{
+			if (numArray[i] > numArray[i+1])
+			{
+				goingDown = true;
+			}
+			else
+			{
+				goingDown = false;
+			}
+		}
+		return goingDown;
 	}	
 }
