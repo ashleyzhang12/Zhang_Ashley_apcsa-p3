f65f9fe0749cf14b9d9db58fe79ad6dbaff0bce3
Ashley Zhang <corgiashley@gmail.com>
2019-02-24 12:09:26 -0800

    Finished TriangleFive

--- Unit07/src/TriangleFiveRunner.java
+++ Unit07/src/TriangleFiveRunner.java
@@ -9,6 +9,14 @@ public class TriangleFiveRunner
    public static void main(String args[])
    {
 	   TriangleFive test = new TriangleFive('c',4);
-	   System.out.print(test);
+	   System.out.println(test);
+	   
+	   test.setLetter('z');
+	   test.setAmount(5);
+	   System.out.println(test);
+	   
+	   test.setLetter('y');
+	   test.setAmount(3);
+	   System.out.println(test);
 	}
 }
