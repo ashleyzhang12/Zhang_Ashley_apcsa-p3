763f993a405d134086ad5f0a4535f5ce6e585c32
Ashley Zhang <corgiashley@gmail.com>
2019-03-13 22:32:53 -0700

    Finished Rational

--- Unit11/src/Rational.java
+++ Unit11/src/Rational.java
@@ -50,6 +50,10 @@ class Rational implements Comparable<Rational>
 		int num2 = other.getNumerator();
 		int den1 = this.getNumerator();
 		int den2 = other.getDenominator();
+		int newNum = num1*den2 + num2 *den1;
+		int newDen = den1*den2;
+		numerator = newNum;
+		denominator = newDen;
 		
 		
 		
@@ -60,7 +64,12 @@ class Rational implements Comparable<Rational>
 
 	private void reduce()
 	{
-		
+		for(int i= 1; i <= Math.max(numerator, denominator);i++)
+		{
+			int divisor = gcd(numerator,denominator);
+			numerator = numerator / divisor;
+			denominator = denominator /divisor;
+		}
 
 
 
@@ -68,16 +77,27 @@ class Rational implements Comparable<Rational>
 
 	private int gcd(int numOne, int numTwo)
 	{
-		int gcd = 0;
-		
-
-
+		int greatest = 0;
+		for(int i = 1; i <= Math.min(numOne, numTwo); i++)
+		{
+			if(numOne % i ==0 && numTwo %i ==0)
+			{
+				if(i > greatest)
+				{
+					greatest = i;
+				}
+			}
+		}
+		if (greatest > 0)
+		return greatest;
+		else
 		return 1;
 	}
 
 	public Object clone ()
 	{
-		return "";
+		Rational clone = new Rational(numerator,denominator);
+		return clone;
 	}
 
 
@@ -96,22 +116,38 @@ class Rational implements Comparable<Rational>
 	
 	public boolean equals( Object obj)
 	{
-
-
+		Rational obj1 = (Rational)obj;
+		this.reduce();
+		obj1.reduce();
+		if(this == obj1)
+		{
+			return true;
+		}
+		else
 		return false;
 	}
 
 	public int compareTo(Rational other)
 	{
-
-
+		int difference = 0;
+		difference = this.getNumerator() * other.getDenominator() - other.getNumerator() * this.getDenominator();
+		if(difference > 0)
+		return 1;
+		else
 		return -1;
+		
 	}
 
 
 
 	
 	//write  toString() method
+	public String toString()
+	{
+		String output = "";
+		output = output + this.getNumerator() + " / " + this.getDenominator();
+		return output;
+	}
 	
 	
 }
--- Unit11/src/RationalRunner.java
+++ Unit11/src/RationalRunner.java
@@ -8,8 +8,7 @@ public class RationalRunner
 {
 	public static void main( String args[] )
 	{
-		/*uncomment once Rational class is built
-		 *
+		
 		Rational test = new Rational();
 		out.println("test = " + test);
 
@@ -65,6 +64,6 @@ public class RationalRunner
 		out.println("\n\n1/6.equals(2/8) = "+rOne.equals(rTwo));				
 		rOne.add(rTwo);
 		out.println("\n\n1/6.add(2/8) = "+ rOne);		
-		*/	
+		
 	}
 }
